5c7c135a175f498ef99de88f764659dad21a7b88
motahhar <motahharmokfi@gmail.com>
2021-09-13 22:32:09 +0430

    add bootstrap and index.html

--- web/views.py
+++ web/views.py
@@ -1,4 +1,15 @@
+from django.core import paginator
 from django.shortcuts import render
+from django.core.paginator import Paginator
+from . import models
+context = {}
+context['message'] = 'این نسخه بتا می‌باشد. | motahharmokfi@gmail.com'
 
 def index_view(request):
-  return render(request, 'index.html')
+  coures_models = models.course_model.objects.all()
+  page = paginator.Paginator(coures_models, 5)
+  page_num = request.GET.get('page', 1)
+  courses = page.page(page_num)
+  context['courses'] = courses
+  return render(request, 'index.html', context)
+  return render(request, 'index.html', context)
